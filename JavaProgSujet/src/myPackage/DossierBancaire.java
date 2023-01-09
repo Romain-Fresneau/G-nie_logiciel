08703b37e81d55030d149c3dff9bd65283390334
maaxcode <maximedoyen55@gmail.com>
2023-01-09 18:02:31 +0100

    classe implémentées

--- JavaProgSujet/src/myPackage/DossierBancaire.java
+++ JavaProgSujet/src/myPackage/DossierBancaire.java
@@ -2,15 +2,33 @@ package myPackage;
 
 public class DossierBancaire {
 	
+	  private double m_soldetotal;
+	  private Compte_courant cc;
+	  private Compte_epargne ce;
+
 	//Constructeur
     public DossierBancaire()
     {
-    	m_solde=0;
+    	m_soldetotal=0;
+    	cc = new Compte_courant(0);
+    	ce = new Compte_epargne(0);
     }
 
-    public void deposer(double value) {m_solde+=value;}
-    public double get_solde() {return m_solde;}
-    public void remunerer() {}
+    public void deposer(double value) 
+    {
+    	cc.Addsolde(0.4*value);
+    	ce.Addsolde(0.6*value);
+    	
+    }
+    public double Consulter() {
+    	m_soldetotal=cc.getsolde()+ce.getsolde();
+    	
+    	return m_soldetotal;
+    	}
+    
+    public void remunerer() {
+    	ce.Remunérer();
+    }
 	
-    private double m_solde;
+  
 }
